70e1316609cc11e70aa3f6cd670734556f09dcc3
Philippe Vo <philippe.vo.nam@gmail.com>
2024-02-10 16:38:03 -0500

    nice looking resume

---
+++ node_modules/jsonresume-theme-even/dist/index.d.ts
@@ -0,0 +1,5 @@
+export namespace pdfRenderOptions {
+    let mediaType: string;
+}
+export function render(resume: import('./schema.d.ts').ResumeSchema): string;
+//# sourceMappingURL=index.d.ts.map
